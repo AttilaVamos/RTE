5fa1088a6ecef64221a295a2fdf88074b0ee6874
Attila Vamos <attila.vamos@gmail.com>
2021-03-24 12:14:31 +0000

    Merge HPCC-HPCC-25341-impr-7.12.x/PR-14704 '--preAbort' on Wed 24 Mar 12:14:31 GMT 2021
    Signed-off-by: Attila Vamos <attila.vamos@gmail.com>

--- hpcc/util/util.py
+++ hpcc/util/util.py
@@ -223,10 +223,26 @@ def abortWorkunit(wuid, taskId = -1, engine = None):
                 pass
             else:
                 err = Error("7100")
-                logger.error("%s. clearOSCache error:%s" % (taskId,  err))
+                logger.error("%s. generateStackTrace error:%s" % (taskId,  err))
                 logger.error(traceback.format_exc())
                 raise Error(err)
                 pass
+
+        if gConfig.preAbort != None:
+            try:
+                logger.error("%3d. Execute pre abort script '%s'", taskId, str(gConfig.preAbort))
+                outFile = os.path.expanduser(gConfig.logDir) + '/' + wuid +'-preAbort.log'
+
+                command=gConfig.preAbort + " > " + outFile + " 2>&1"
+                myProc = subprocess.Popen([ command ],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+                result = myProc.stdout.read() + myProc.stderr.read()
+
+                logger.debug("%3d. Pre abort script result '%s'", taskId, wuid, str(result))
+                logger.error("%3d. Pre abort script result stored into '%s'", taskId, outFile)
+            except Exception as e:
+                printException("preAbort scrip:" + repr(e),  True)
+                logger.error("%3d. Exception in executing pre abort script: '%s'", taskId, repr(e))
+            pass
 
         shell = Shell()
         cmd = 'ecl'
@@ -240,7 +256,7 @@ def abortWorkunit(wuid, taskId = -1, engine = None):
         state=shell.command(cmd, *defaults)(*args)
     else:
         logger.error("%3d. abortWorkunit(wuid:'%s', engine:'%s') invalid WUID.", taskId, wuid, str(engine))
-        
+
     return state
 
 def createZAP(wuid, taskId,  reason=''):
